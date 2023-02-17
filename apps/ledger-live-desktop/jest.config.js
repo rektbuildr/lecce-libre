1f92fd7298b03a4c6183858913f0e767c6bfbcc3
Gregor Gilchrist <gregor.gilchrist@ledger.fr>
2023-02-17 11:43:20 +0100

    feat: WIPetty WiP whippp

--- apps/ledger-live-desktop/jest.config.js
+++ apps/ledger-live-desktop/jest.config.js
@@ -1,9 +1,32 @@
-module.exports = {
+const defaultConfig = {
   globals: {
-    __DEV__: false,
-    __APP_VERSION__: "2.0.0",
+    __DEV__: false, // LL specific or Node global variable?
+    __APP_VERSION__: "2.0.0", // ?
+    "ts-jest": {
+      isolatedModules: true,
+    },
   },
-  globalSetup: "<rootDir>/tests/setup.js",
+  testEnvironment: "node",
+  globalSetup: "<rootDir>/tets/setup.js",
   setupFiles: ["<rootDir>/tests/jestSetup.js"],
-  testEnvironment: "jsdom",
+  moduleDirectories: ["node_modules"],
+};
+
+module.exports = {
+  projects: [
+    {
+      // config for normal jest unit tests (not react testing library)
+      ...defaultConfig,
+      testPathIgnorePatterns: ["(/__tests__/.*|(\\.|/)react\\.test|spec)\\.tsx"], // this basically says don't run this config for any spec/test files matching this regex
+    },
+    {
+      ...defaultConfig,
+      displayName: "dom", // ?
+      testEnvironment: "jsdom",
+      testRegex: "(/__tests__/.*|(\\.|/)react\\.test|spec)\\.tsx", // run these specific tests
+      moduleNameMapper: {
+        uuid: require.resolve("uuid"), // finds the path of the uuid modile - more overhead to look this up but readable and easy
+      },
+    },
+  ],
 };
