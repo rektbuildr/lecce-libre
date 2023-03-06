897013d839f6535072d58a59a82cb3178f0af739
Juan Cortés Ross <juan.cortes.ross@gmail.com>
2023-03-06 17:20:53 -0100

    poc

--- apps/ledger-live-desktop/src/renderer/components/OperationsList/OperationDate.tsx
+++ apps/ledger-live-desktop/src/renderer/components/OperationsList/OperationDate.tsx
@@ -1,9 +1,9 @@
-// @flow
-import React, { PureComponent } from "react";
+import React from "react";
 import styled from "styled-components";
 import Text from "~/renderer/components/Text";
+import useDateTimeFormat from "~/renderer/hooks/useDateTimeFormat";
+
 import type { ThemedComponent } from "~/renderer/styles/StyleProvider";
-import FormattedDate from "../FormattedDate";
 
 const Hour: ThemedComponent<{}> = styled(Text).attrs(() => ({
   color: "palette.text.shade60",
@@ -14,13 +14,14 @@ const Hour: ThemedComponent<{}> = styled(Text).attrs(() => ({
   text-transform: uppercase;
 `;
 
-export default class OperationDate extends PureComponent<{ date: Date }> {
-  render() {
-    const { date } = this.props;
-    return (
-      <Hour>
-        <FormattedDate date={date} format="LT" />
-      </Hour>
-    );
-  }
-}
+type Props =  { date: Date };
+const OperationDate = ({ date }: Props) => {
+  const dateFormatter = useDateTimeFormat();
+  return (
+    <Hour>
+      <div>{dateFormatter(date)}</div>
+    </Hour>
+  );
+};
+
+export default OperationDate;
