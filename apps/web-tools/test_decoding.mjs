c74f3b5472ca474139f8699b7fd1316221cd59f6
Julien Nicoulaud <julien.nicoulaud@ledger.fr>
2023-11-30 14:54:12 +0100

    fix decoder order

--- apps/web-tools/test_decoding.mjs
+++ apps/web-tools/test_decoding.mjs
@@ -7,12 +7,12 @@ import ExchangeSetPartnerPublicKeyResponse from './payloads/ExchangeSetPartnerPu
 import { KaitaiStream } from "kaitai-struct";
 
 const decoders = [
-  s => new ExchangeGetVersionRequest(s),
-  s => new ExchangeGetVersionResponse(s),
   s => new ExchangeStartTransactionRequest(s),
-  s => new ExchangeStartTransactionResponse(s),
   s => new ExchangeSetPartnerPublicKeyRequest(s),
-  s => new ExchangeSetPartnerPublicKeyResponse(s)
+  s => new ExchangeGetVersionRequest(s),
+  s => new ExchangeSetPartnerPublicKeyResponse(s),
+  s => new ExchangeStartTransactionResponse(s),
+  s => new ExchangeGetVersionResponse(s)
 ]
 
 function tryDecode(payload) {
@@ -26,8 +26,11 @@ function tryDecode(payload) {
   return undefined;
 }
 
-const payload = Buffer.from("e003000100", "hex");
-const message = tryDecode(payload);
+const start_transaction_request = Buffer.from("e003010000", "hex");
+const start_transaction_response = Buffer.from("42505a4f4d4f5348504e9000", "hex");
+const set_partner_public_key_request = Buffer.from("e00401004b094368616e67656c6c790480d7c0d3a9183597395f58dda05999328da6f18fabd5cda0aff8e8e3fc633436a2dbf48ecb23d40df7c3c7d3e774b77b4b5df0e9f7e08cf1cdf2dba788eb085b", "hex");
+const set_partner_public_key_response = Buffer.from("9000", "hex");
+const message = tryDecode(set_partner_public_key_request);
 
 if (message == undefined) {
   console.error('failed decoding');
