LedgerHQ/ledger-live

chore: featureflag refactoring

apps/ledger-live-desktop/src/renderer/components/FirebaseRemoteConfig.tsx
@@ -1,32 +1,13 @@
-import React, { ReactNode, useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { initializeApp } from "firebase/app";
 import { getRemoteConfig, fetchAndActivate, RemoteConfig } from "firebase/remote-config";
-import { DEFAULT_FEATURES } from "@ledgerhq/live-common/featureFlags/index";
-import { Features } from "@ledgerhq/types-live";
-import reduce from "lodash/reduce";
-import snakeCase from "lodash/snakeCase";
+import { DEFAULT_FEATURES, formatDefaultFeatures } from "@ledgerhq/live-common/featureFlags/index";
+import type { FirebaseFeatureFlagsProviderProps as Props } from "@ledgerhq/live-common/featureFlags/index";
 import { getFirebaseConfig } from "~/firebase-setup";
 
 export const FirebaseRemoteConfigContext = React.createContext<RemoteConfig | null>(null);
 
 export const useFirebaseRemoteConfig = () => useContext(FirebaseRemoteConfigContext);
-
-export const formatToFirebaseFeatureId = (id: string) => `feature_${snakeCase(id)}`;
-
-// Firebase SDK treat JSON values as strings
-const formatDefaultFeatures = (config: Features) =>
-  reduce(
-    config,
-    (acc, feature, featureId) => ({
-      ...acc,
-      [formatToFirebaseFeatureId(featureId)]: JSON.stringify(feature),
-    }),
-    {},
-  );
-
-type Props = {
-  children?: ReactNode;
-};
 
 export const FirebaseRemoteConfigProvider = ({ children }: Props): JSX.Element | null => {
   const [config, setConfig] = useState<RemoteConfig | null>(null);

rektbuildr/lecce-libre

apps/ledger-live-desktop/src/renderer/components/FirebaseRemoteConfig.tsx
@@ -1,11 +1,8 @@
 import React, { useContext, useEffect, useState } from "react";
 import { initializeApp } from "firebase/app";
 import { getRemoteConfig, fetchAndActivate, RemoteConfig, getValue } from "firebase/remote-config";
-import {
-  DEFAULT_FEATURES,
-  formatDefaultFeatures,
-  LiveConfig,
-} from "@ledgerhq/live-config/featureFlags/index";
+import { LiveConfig } from "@ledgerhq/live-config/LiveConfig";
+import { DEFAULT_FEATURES, formatDefaultFeatures } from "@ledgerhq/live-config/featureFlags/index";
 import type { FirebaseFeatureFlagsProviderProps as Props } from "@ledgerhq/live-config/featureFlags/index";
 import { getFirebaseConfig } from "~/firebase-setup";
 